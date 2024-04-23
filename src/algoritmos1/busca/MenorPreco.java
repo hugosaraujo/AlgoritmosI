e6df266d9de77aff9d0ed91c3bfc7b747c0bc53a
hugo de araujo <hugo.saraujo@tutanota.com>
2024-04-23 19:18:09 -0300

    Refatoração, criação de funções para buscar valores de carros.

--- src/algoritmos1/busca/MenorPreco.java
+++ src/algoritmos1/busca/MenorPreco.java
@@ -8,27 +8,41 @@ public class MenorPreco {
     public static void main(String[] args) {
 
         int totalDeCarros = 5;
-        Carro precosCarros[] = new Carro[totalDeCarros];
-        precosCarros[0] = new Carro("Lamborguini", 1000000);
-        precosCarros[1] = new Carro("Jipe", 46000);
-        precosCarros[2] = new Carro("Brasília", 16000);
-        precosCarros[3] = new Carro("Smart", 46000);
-        precosCarros[4] = new Carro("Fusca", 17000);
+        Carro precosCarros[] = {
+                new Carro("Lamborguini", 1000000),
+                new Carro("Jipe", 46000),
+                new Carro("Brasília", 16000),
+                new Carro("Smart", 46000),
+                new Carro("Fusca", 17000)
+        };
 
-        int menorPreco = 0;
-        int maiorPreco = 0;
+        int menorPreco = buscaMenor(precosCarros);
+        int maiorPreco = buscarMaior(precosCarros);
 
-        for (int atual = 0; atual < totalDeCarros; atual++) {
-            if(precosCarros[atual].getValor() < precosCarros[menorPreco].getValor()){
-                menorPreco = atual;
-            } else if (precosCarros[atual].getValor() > precosCarros[maiorPreco].getValor()){
-                maiorPreco = atual;
-            }
-        }
         System.out.printf("Carro da posição %d \n",menorPreco);
         System.out.printf(precosCarros[menorPreco].toString());
         System.out.printf("Carro da posição %d \n",maiorPreco);
         System.out.printf(precosCarros[maiorPreco].toString());
 
+    }
+
+    public static int buscarMaior(Carro[] precosCarros) {
+        int maisCaro = 0;
+        for (int atual = 0; atual < precosCarros.length; atual++) {
+            if (precosCarros[atual].getValor() > precosCarros[maisCaro].getValor()){
+                maisCaro = atual;
+            }
+        }
+        return maisCaro;
+    }
+
+    public static int buscaMenor(Carro[] precosCarros) {
+        int maisBarato = 0;
+        for (int atual = 0; atual < precosCarros.length; atual++) {
+            if(precosCarros[atual].getValor() < precosCarros[maisBarato].getValor()){
+                maisBarato = atual;
+            }
+        }
+        return maisBarato;
     }
 }
